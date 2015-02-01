a882ad9e8bbe26686ba78742a5814877030a39f0
Ramazan Akkas <ramazan.akkas@mni.thm.de>
2015-02-02 00:05:59 +0100

    update

--- js/mein.js
+++ js/mein.js
@@ -2,3 +2,31 @@ $(document).ready(function() {
 	$("#dragme").draggable();
 });
 
+
+	var valid=function(id){
+		var search=document.getElementById("search").value;
+		if(search.length===0||search.length>=50||nurLeerzeichen(search)){
+			$(document).ready(function(){
+				alert("Keine gültige Eingabe!");
+			});
+		}
+		else{
+			if(id===0){
+				var i=document.getElementById("wikiform");
+				i.action="http://de.wikipedia.org/wiki/Spezial:Suche?fulltext="+search;
+				i.submit();
+			}
+			else {
+				var i=document.getElementById("wikiform");
+				i.action="http://de.wikipedia.org/wiki/Spezial:Suche?"+search;
+				i.submit();
+			}
+		}
+	}
+	var nurLeerzeichen=function(j){
+		for (var i=0;i<j.length;i++){
+			if(!(j[i]===" "))return false;
+		}
+		return true;
+	}
+
